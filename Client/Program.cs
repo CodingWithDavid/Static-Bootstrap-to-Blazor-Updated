02edf65f4f3c1027eeb5be3b5058e8a59f0577d4
David Gallivan <33528347+CodingWithDavid@users.noreply.github.com>
2021-02-17 13:51:18 -0700

    First set of changes

--- Client/Program.cs
+++ Client/Program.cs
@@ -4,6 +4,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
+//3rd party
+using Blazored.Toast;
+using Blazored.Modal;
+
 namespace BlazorApp.Client
 {
     public class Program
@@ -15,7 +19,8 @@ namespace BlazorApp.Client
 
             var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
             builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
-
+            builder.Services.AddBlazoredModal();
+            builder.Services.AddBlazoredToast();
             await builder.Build().RunAsync();
         }
     }
